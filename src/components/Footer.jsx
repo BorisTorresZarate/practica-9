8d5d0ca8a53a960a8dc65e8958fdc2f366fb3c83
BorisTorres <bogui231297@gmail.com>
2024-05-30 12:31:12 -0500

    primer commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+
+export function Footer () {
+  // const { filters } = useFilters()
+
+  return (
+    <footer className='footer'>
+      <h4>Boris Torres ⚛️ － <span>@bogui231297@gmail.com</span></h4>
+      <h5>Shopping Cart </h5>
+    </footer>
+  )
+}
